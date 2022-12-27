4decd2051cb762df40fea7f30d9c8a7ced845e12
Fernando <fernando@manso.com.br>
2022-12-27 17:55:58 -0300

    split models to single files

---
+++ src/models/Contract.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize')
+
+class Contract extends Sequelize.Model {}
+
+module.exports = (sequelize) =>
+  Contract.init(
+    {
+      terms: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM('new', 'in_progress', 'terminated'),
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Contract',
+    }
+  )
---
+++ src/models/index.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite3',
+})
+
+const { Profile } = require('./Profile')(sequelize)
+const { Contract } = require('./Contract')(sequelize)
+const { Job } = require('./Job')(sequelize)
+
+Profile.hasMany(Contract, { as: 'Contractor', foreignKey: 'ContractorId' })
+Contract.belongsTo(Profile, { as: 'Contractor' })
+Profile.hasMany(Contract, { as: 'Client', foreignKey: 'ClientId' })
+Contract.belongsTo(Profile, { as: 'Client' })
+Contract.hasMany(Job)
+Job.belongsTo(Contract)
+
+module.exports = {
+  sequelize,
+  Profile,
+  Contract,
+  Job,
+}
