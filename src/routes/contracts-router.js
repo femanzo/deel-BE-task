efbac27d7a5559ff69e8411a5ae94c8893e7177a
Fernando <fernando@manso.com.br>
2022-12-28 02:50:48 -0300

    renamed auth middleware

--- src/routes/contracts-router.js
+++ src/routes/contracts-router.js
@@ -1,10 +1,10 @@
 const { Router } = require('express')
 const { contractsController } = require('../controllers')
-const { getProfile } = require('../middlewares')
+const { authMiddleware } = require('../middlewares')
 
 const router = Router()
 
-router.get('/', getProfile, contractsController.getUserNonTerminantedContracts)
-router.get('/:id', getProfile, contractsController.getUserContractById)
+router.get('/', authMiddleware, contractsController.getUserNonTerminantedContracts)
+router.get('/:id', authMiddleware, contractsController.getUserContractById)
 
 module.exports = router
