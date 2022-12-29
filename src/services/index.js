05a030ccd375373ecf94da6c53e96ebb99915d73
Fernando <fernando@manso.com.br>
2022-12-29 19:37:45 -0300

    exported contractServices

--- src/services/index.js
+++ src/services/index.js
@@ -2,4 +2,5 @@ module.exports = {
   jobServices: require('./job-services'),
   profileServices: require('./profile-services'),
   adminServices: require('./admin-services'),
+  contractServices: require('./contract-services'),
 }
